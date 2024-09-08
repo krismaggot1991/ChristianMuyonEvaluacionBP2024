76488783a30440f418f4c7d7668c359bfe4aab01
Christian Alfredo Muyon Rivera <christian.muyon.rivera@gmail.com>
2024-09-08 12:21:22 -0500

    Client module: saveClient service

--- client/src/main/java/com/pichincha/client/service/impl/ClientServiceImpl.java
+++ client/src/main/java/com/pichincha/client/service/impl/ClientServiceImpl.java
@@ -1,19 +1,31 @@
 package com.pichincha.client.service.impl;
 
+import static lombok.AccessLevel.PRIVATE;
+
+import com.pichincha.client.repository.ClientRepository;
 import com.pichincha.client.service.ClientService;
 import com.pichincha.client.service.dto.ClientDto;
+import com.pichincha.client.service.mapper.ClientMapper;
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(level = PRIVATE, makeFinal = true)
 public class ClientServiceImpl implements ClientService {
+
+  ClientRepository clientRepository;
+  ClientMapper clientMapper;
 
   @Override
   public void saveClient(ClientDto clientDto) {
-
+    log.info("Add new client with identification: {}", clientDto.getIdentification());
+    clientRepository.save(clientMapper.toEntity(clientDto));
   }
 
   @Override
